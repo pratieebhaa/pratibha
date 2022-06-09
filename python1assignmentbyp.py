24988ec96837cf9d0c12d736ea13c1afa21ca4b5
pratieebhaa <105800949+pratieebhaa@users.noreply.github.com>
2022-06-09 17:35:00 +0530

    Add files via upload

---
+++ python1assignmentbyp.py
@@ -0,0 +1,62 @@
+1. In the below elements which of them are values or an expression? eg:- values can be integer or string and expressions will be mathematical operators.
+*
+ans= expression(mathematical operator)
+'hello'
+ans= value(string)
+-87.8
+ans= value(foat)
+-
+ans= expression(mathematical operator)
+/
+ans= expression(mathematical operator)
++
+ans= expression(operator)
+6
+ans= value(integer)
+
+2. What is the difference between string and variable?
+ans= string is itself a datatype in python and the value which can be stored in the variable.
+eg: 'this is string'
+variable is used for storing the values in a variable and variables are need not to be declared at the start in python.
+a=10
+
+3. Describe three different data types.
+ans=data types or the class is same, data can be either a numbers,strings and etc. here, there are majorly 3 data types used in pyhton
+1:string contains the string of letters in it,it can be stored as a value in a variable.
+there are three types to write a sting, it can be enclosed in ("")double quotes,('')sindle quotes or three of each('''or""").
+2:integer are numeric terms such as whole number,positive or negative, without decimals, of unlimited length,e.g.0,100,-10. it can be stored as values in a variable,
+x=10
+3:float type are the numbers which can be positive , negative real numbers with a fractional part denoted by decimal symbol(.),e.g. 123.45,-2334.44.
+f=1.34
+type(f)
+"float"
+4. What is an expression made up of? What do all expressions do?
+ans=expression is made up of variable  ,values and the operator, and the expression is used for evaluation.
+x=8
+z=x+4
+z=12
+5. This assignment statements, like spam = 10. What is the difference between an expression and a statement?
+ans:spam=10 , the expression evaluates to a value and statement executes something 
+
+6. After running the following code, what does the variable bacon contain?
+bacon = 22
+bacon + 1
+ans= 23
+
+7. What should the values of the following two terms be?
+'spam' + 'spamspam'
+'spam' * 3
+ans='spamspamspam'
+
+8. Why is eggs a valid variable name while 100 is invalid? 
+ans: variable names can only have name,number and underscore,but only number can't be used as variable name it should start with name or underscore only.
+_name1,name1,name=valid variables
+9. What three functions can be used to get the integer, floating-point number, or string version of a value?
+ans: for integer :-int()
+for floating point number:-float()
+for string:-str()
+10. Why does this expression cause an error? How can you fix it?
+'I have eaten ' + 99 + ' burritos.'
+ans= string can concatenate string only not integer,
+we can fix the error by replacing 99 to "99"
+
